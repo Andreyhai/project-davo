862c9239f7631571d76b85e22b4995eb1557cff7
Andrey <ustavitski.andrei@gail.com>
2024-06-08 17:12:11 +0300

    structure

---
+++ front/src/components/sections/first/FirstSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './FirstSection.module.css';
+import bg from '../../../sources/bg.png'
+const FirstSection = () => {
+    return (
+        <section>
+            <img src={bg} alt="image"/>
+        </section>
+    );
+};
+
+export default FirstSection;
